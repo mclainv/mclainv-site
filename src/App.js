b3fe01dffd7e1491f54241d99fc3cd9f7e1f6032
mclainv <robert.adams22@capefearacademy.org>
2021-09-18 20:50:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ReactPlayer url='https://www.youtube.com/watch?v=MXNbfU0Ww_E'></ReactPlayer>
+    </div>
+  );
+}
+
+export default App;
